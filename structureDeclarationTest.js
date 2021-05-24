9f7a14a606111eea1c39d994684b1874d0404667
teo.samarzija <teo.samarzija@gmail.com>
2021-05-24 20:53:26 +0200

    Made `structureDeclarationTest.js` return appropriate exit codes.

--- structureDeclarationTest.js
+++ structureDeclarationTest.js
@@ -10,9 +10,10 @@ WebAssembly.instantiate(buffer, importObject).then((results) => {
   if (exports.foo() == 3 && exports.bar() == 12 && exports.spam() == 3 &&
       exports.eggs() == 3 && exports.onion() == 1 &&
       exports.pomegranate() == 12 && exports.spaghetti() == 2 &&
-      exports.elephant() == 1 && exports.arrow() == 2)
+      exports.elephant() == 1 && exports.arrow() == 2) {
+    process.exitCode = 0;
     console.log("Structure declaration test succeeded!");
-  else
+  } else {
     console.log("Structure declaration test failed!\n" +
                 "AEC function\tExpected Return Value\tActual Return Value\n" +
                 "foo()\t3\t" + exports.foo() + "\n" +
@@ -24,4 +25,6 @@ WebAssembly.instantiate(buffer, importObject).then((results) => {
                 "spaghetti()\t2\t" + exports.spaghetti() + "\n" +
                 "elephant()\t1\t" + exports.elephant() + "\n" +
                 "arrow()\t2\t" + exports.arrow() + "\n");
+    process.exitCode = 1;
+  }
 });
