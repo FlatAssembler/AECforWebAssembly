8fd5138dd46605bbee6efcea1b9f8abc49b806a1
Teo Samaržija <teo.samarzija@gmail.com>
2021-08-26 17:26:57 +0200

    `namedArgumentsTest` finally passes!

--- namedArgumentsTest.js
+++ namedArgumentsTest.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 const buffer = fs.readFileSync("namedArgumentsTest.wasm");
-WebAssembly.instantiate(buffer).then((results) => {
-  const exports = results.instance.exports;
-  if (exports.namedArgumentsTest() === 0) {
-    process.exitCode = 0;
-    console.log("Named Arguments Test succeeded!");
-  } else {
-    process.exitCode = exports.namedArgumentsTest();
-    console.log(
-        "Named Arguments Test failed, \"namedArgumentsTest\" returned the error code " +
-        exports.namedArgumentsTest() + "!");
-  }
-});
+WebAssembly
+    .instantiate(
+        buffer,
+        {JavaScript : {printFloat32 : function(f32) { console.log(f32); }}})
+    .then((results) => {
+      const exports = results.instance.exports;
+      if (exports.namedArgumentsTest() === 0) {
+        process.exitCode = 0;
+        console.log("Named Arguments Test succeeded!");
+      } else {
+        process.exitCode = exports.namedArgumentsTest();
+        console.log(
+            "Named Arguments Test failed, \"namedArgumentsTest\" returned the error code " +
+            exports.namedArgumentsTest() + "!");
+      }
+    });
