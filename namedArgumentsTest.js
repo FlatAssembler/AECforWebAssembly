e84374e42ff6daadf167e55df546d4c1a8bf9e0f
Teo Samaržija <teo.samarzija@gmail.com>
2021-08-26 16:38:34 +0200

    Attempting to diagnose the error in `namedArgumentsTest`.

--- namedArgumentsTest.js
+++ namedArgumentsTest.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const buffer = fs.readFileSync("namedArgumentsTest.wasm");
-WebAssembly.instantiate(buffer).then((results)=>{
+WebAssembly.instantiate(buffer).then((results) => {
   const exports = results.instance.exports;
-  if (exports.namedArgumentsTest() === 1) {
+  if (exports.namedArgumentsTest() === 0) {
     process.exitCode = 0;
     console.log("Named Arguments Test succeeded!");
-  }
-  else {
-    process.exitCode = 1;
-    console.log("Named Arguments Test failed!");
+  } else {
+    process.exitCode = exports.namedArgumentsTest();
+    console.log(
+        "Named Arguments Test failed, \"namedArgumentsTest\" returned the error code " +
+        exports.namedArgumentsTest() + "!");
   }
 });
