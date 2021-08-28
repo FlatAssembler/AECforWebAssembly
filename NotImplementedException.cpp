f1458e538c713179eeb17858bfe73be3938dd41c
vekma solazarn <89711844+clydenovac@users.noreply.github.com>
2021-08-29 01:29:18 +0200

    add some tidying to not implemented exception
    you can use keyword final when u dont want to inherit the class anymore
    also if u put noexcept then you guarantee exceptionless ctor

--- NotImplementedException.cpp
+++ NotImplementedException.cpp
@@ -1,7 +1,7 @@
 #include <stdexcept>
 
 #pragma once
-class NotImplementedException : public std::runtime_error {
+class NotImplementedException final : public std::runtime_error {
 public:
-  NotImplementedException(std::string what) : std::runtime_error(what) {}
+  NotImplementedException(std::string what) noexcept : std::runtime_error(what) {}
 };
