adc12a84444dfdf648e30514e1102cd3d759f872
vekma solazarn <89711844+clydenovac@users.noreply.github.com>
2021-08-29 01:30:33 +0200

    also const str ref

--- NotImplementedException.cpp
+++ NotImplementedException.cpp
@@ -3,5 +3,5 @@
 #pragma once
 class NotImplementedException final : public std::runtime_error {
 public:
-  NotImplementedException(std::string what) noexcept : std::runtime_error(what) {}
+  NotImplementedException(const std::string& what) noexcept : std::runtime_error(what) {}
 };
