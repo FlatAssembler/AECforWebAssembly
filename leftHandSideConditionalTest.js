eea826f67a18a8b9234b65270a4f180d423bdb66
Teo Samaržija <teo.samarzija@gmail.com>
2021-09-10 20:39:26 +0200

    Added the Left Hand Side Conditional Test.

---
+++ leftHandSideConditionalTest.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const buffer = fs.readFileSync("leftHandSideConditionalTest.wasm");
+WebAssembly.instantiate(buffer).then((results) => {
+  const exports = results.instance.exports;
+  if (exports.gcd(25, 80) === 5 && exports.gcd(80, 25) === 5 &&
+      exports.gcd(8, 12) === 4 && exports.gcd(12, 8) === 4 &&
+      exports.gcd(54, 24) === 6) {
+    process.exitCode = 0;
+    console.log("Left Hand Side Conditional Test succeeded!");
+  } else {
+    process.exitCode = 1;
+    console.log("Left Hand Side Conditional Test failed!");
+  }
+});
