f5295a7466cd380d6c8b7024fe4e2a1c826fdd2c
Teo Samaržija <teo.samarzija@gmail.com>
2021-09-10 20:53:26 +0200

    Added another test example to `leftHandSideConditionalTest.js`

--- leftHandSideConditionalTest.js
+++ leftHandSideConditionalTest.js
@@ -4,7 +4,7 @@ WebAssembly.instantiate(buffer).then((results) => {
   const exports = results.instance.exports;
   if (exports.gcd(25, 80) === 5 && exports.gcd(80, 25) === 5 &&
       exports.gcd(8, 12) === 4 && exports.gcd(12, 8) === 4 &&
-      exports.gcd(54, 24) === 6) {
+      exports.gcd(54, 24) === 6 && exports.gcd(48, 18) === 6) {
     process.exitCode = 0;
     console.log("Left Hand Side Conditional Test succeeded!");
   } else {
