03676af09ddd5b3a1c27f9516f66b2b93042e4c4
Teo Samaržija <teo.samarzija@gmail.com>
2025-09-17 10:39:39 +0200

    Wrote the JS and CMake of the Break and Continue Test

---
+++ break_and_continue_test.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const buffer = fs.readFileSync("break_and_continue_test.wasm");
+WebAssembly.instantiate(buffer).then((results) => {
+  const exports = results.instance.exports;
+  if (exports.test() === 124) {
+    process.exitCode = 0;
+    console.log("Break and Continue Test succeeded!");
+  } else {
+    process.exitCode = 1;
+    console.log("Break and Continue Test failed!");
+  }
+});
