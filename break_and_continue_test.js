4fd7d5c83d4d20571714caa5f25bfe0a23f43b8a
Teo Samaržija <teo.samarzija@gmail.com>
2025-09-19 14:03:29 +0200

    Tested my fix to #25

--- break_and_continue_test.js
+++ break_and_continue_test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const buffer = fs.readFileSync("break_and_continue_test.wasm");
 WebAssembly.instantiate(buffer).then((results) => {
   const exports = results.instance.exports;
-  if (exports.test() === 124) {
+  if (exports.test() === 124 && exports.issue25() === 1) {
     process.exitCode = 0;
     console.log("Break and Continue Test succeeded!");
   } else {
