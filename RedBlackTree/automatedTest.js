f72b19fbb1dc9b76837179adcc35a2c856a328ea
Teo Samarzija <teo.samarzija@gmail.com>
2026-04-28 14:54:22 +0200

    Added another Red-Black Tree test

--- RedBlackTree/automatedTest.js
+++ RedBlackTree/automatedTest.js
@@ -1,30 +1,42 @@
 const rbt_api = require('./rbt_api.js');
 const fs = require('fs');
 const wasm = fs.readFileSync("RedBlackTree.wasm");
-WebAssembly.instantiate(wasm, { wasi_unstable: {
-          printString: function(){},
-          clearScreen: function(){},
-          noMoreFreeMemory: function(){},
-          segmentationFault: function(){},
-          getLengthOfTheInput: function(){},
-          getCharacterOfTheInput: function(){},
-          drawRectangle: function(){},
-          drawLine: function(){},
-          drawText: function(){},
-          setDiagramWidth: function(){},
-          setDiagramHeight: function(){},
-        },
-      }).then((module)=>{
-	rbt_api.RBTAPI.init(module).then((rbtapi)=>{
-	const preorder = rbtapi.setKeysRenderAndGetPreorder([1, 2, 3]);
-	if (preorder == "2 1 3 ") {
-		console.log("The test has passed!");
-		process.exitCode = 0;
-	}
-	else {
-		console.log("The test has failed!");
-		console.log(preorder);
-		process.exitCode = 1;
-	}
-	});
-});
+WebAssembly
+    .instantiate(wasm, {
+      wasi_unstable : {
+        printString : function() {},
+        clearScreen : function() {},
+        noMoreFreeMemory : function() {},
+        segmentationFault : function() {},
+        getLengthOfTheInput : function() {},
+        getCharacterOfTheInput : function() {},
+        drawRectangle : function() {},
+        drawLine : function() {},
+        drawText : function() {},
+        setDiagramWidth : function() {},
+        setDiagramHeight : function() {},
+      },
+    })
+    .then((module) => {
+      rbt_api.RBTAPI.init(module).then((rbtapi) => {
+        let preorder;
+	preorder = rbtapi.setKeysRenderAndGetPreorder([ 1, 2, 3 ]);
+        if (preorder == "2 1 3 ") {
+          console.log("The test has passed!");
+          process.exitCode = 0;
+        } else {
+          console.log("The test has failed!");
+          console.log(preorder);
+          process.exitCode = 1;
+        }
+	preorder = rbtapi.setKeysRenderAndGetPreorder([1, 3, 2, 5, 4, -1, 0]);
+        if (preorder == "2 0 -1 1 4 3 5 ") {
+          console.log("The test has passed!");
+          process.exitCode = 0;
+        } else {
+          console.log("The test has failed!");
+          console.log(preorder);
+          process.exitCode = 1;
+        }
+      });
+    });
